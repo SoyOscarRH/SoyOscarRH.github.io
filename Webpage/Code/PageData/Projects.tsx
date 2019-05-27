2a33b00944ec28d58841ac1291b1774ce3055f9d
Oscar Rosas <soyoscarrh@gmail.com>
2019-05-27 13:08:00 -0500

    Move with by folder + update resume

--- Webpage/Code/PageData/Projects.tsx
+++ Webpage/Code/PageData/Projects.tsx
@@ -16,47 +16,43 @@ export interface ProjectsI {
 }
 
 const Projects: ProjectsI = {
-  CompetitiveReference: {
+  CompilandoConocimiento: {
     Title: (
       <span>
         {" "}
-        <b>Competitive</b>Reference
+        <b>Compilando</b>Conocimiento
       </span>
     ),
-    LinkToProject: "https://github.com/CompilandoConocimiento/Reference",
-    CheckOut: "https://compilandoconocimiento.github.io/Reference/",
-    Color: "blue-grey darken-3",
+    LinkToProject: "https://compilandoconocimiento.com/",
+    CheckOut: "https://compilandoconocimiento.com/",
+    Color: "purple",
     English: (
       <span>
-        This is all my personal implementations of the most famous and useful competitive
-        programming algorithms.
+        Creator of a blog (in spanish) to learn about science in general, discrete math, physics,
+        vectorial analysis, simple algorithms and data structures (stacks, queues, BST, AVL trees,
+        etc), automata theory, intro to C and to object oriented programming using Java. The blog
+        has around 100 000 views since the beginning of 2018.
         <br />
         <br />
-        Most of the implementations are in C++, but you can also find them in C and even in Python
-        and Java.
-        <br />
-        <br />
-        I'm also working on creating a Reference (Book & Website) so many more people can benefit
-        from it, and get further along in this awesome mind-sport.
+        Here you can also find all the books we as an organization are writing for free.
       </span>
     ),
     Spanish: (
       <span>
-        Recopilación de mis implementaciones personales de los más famosos y útiles algoritmos de la
-        programación competitiva.
+        Creador de un blog (en español) para aprender sobre ciencia en general, matemáticas
+        discretas, física, análisis vectorial, algoritmos simples y estructuras de datos (pilas,
+        colas, BST, árboles AVL, etc.), teoría de autómatas, introducción a C y programación
+        orientada a objetos utilizando Java. El blog Tiene alrededor de 100 000 visitas desde
+        principios de 2018.
         <br />
         <br />
-        La mayoría de las implementaciones están en C ++, pero también puedes encontrar algunas en C
-        e incluso en Python y Java.
-        <br />
-        <br />
-        También estoy trabajando en la creación de un "Refence" (libro y sitio web) para que así más
-        personas puedan beneficiarse de esto y avanzar más en este increíble deporte mental.
+        Aquí también se puede encontrar todos los libros que, como organización, escribimos de forma
+        gratuita.
       </span>
     ),
     Tags: {
-      English: ["Competitive Programming", "C++", "Webapp", "React", "Typescript", "Book"],
-      Spanish: ["Programación Competitiva", "C++", "Webapp", "React", "Typescript", "Libro"],
+      English: ["Educational blog", "math", "algorithms"],
+      Spanish: ["Blog de educación", "mate", "algoritmos"],
     },
   },
   CompetitiveSolutions: {
@@ -99,45 +95,6 @@ const Projects: ProjectsI = {
     Tags: {
       English: ["C++", "Competitive Programming", "Solutions", "Online Judges"],
       Spanish: ["C++", "Programación Competitiva", "Soluciones", "Jueces en línea"],
-    },
-  },
-  CompilandoConocimiento: {
-    Title: (
-      <span>
-        {" "}
-        <b>Compilando</b>Conocimiento
-      </span>
-    ),
-    LinkToProject: "https://compilandoconocimiento.com/",
-    CheckOut: "https://compilandoconocimiento.com/",
-    Color: "purple",
-    English: (
-      <span>
-        Creator of a blog (in spanish) to learn about science in general, discrete math, physics,
-        vectorial analysis, simple algorithms and data structures (stacks, queues, BST, AVL trees,
-        etc), automata theory, intro to C and to object oriented programming using Java. The blog
-        has around 100 000 views since the beginning of 2018.
-        <br />
-        <br />
-        Here you can also find all the books we as an organization are writing for free.
-      </span>
-    ),
-    Spanish: (
-      <span>
-        Creador de un blog (en español) para aprender sobre ciencia en general, matemáticas
-        discretas, física, análisis vectorial, algoritmos simples y estructuras de datos (pilas,
-        colas, BST, árboles AVL, etc.), teoría de autómatas, introducción a C y programación
-        orientada a objetos utilizando Java. El blog Tiene alrededor de 100 000 visitas desde
-        principios de 2018.
-        <br />
-        <br />
-        Aquí también se puede encontrar todos los libros que, como organización, escribimos de forma
-        gratuita.
-      </span>
-    ),
-    Tags: {
-      English: ["Educational blog", "math", "algorithms"],
-      Spanish: ["Blog de educación", "mate", "algoritmos"],
     },
   },
   DIYCompiler: {
@@ -464,6 +421,49 @@ const Projects: ProjectsI = {
         "Programación Competitiva",
         "proyecto de una noche",
       ],
+    },
+  },
+  CompetitiveReference: {
+    Title: (
+      <span>
+        {" "}
+        <b>Competitive</b>Reference
+      </span>
+    ),
+    LinkToProject: "https://github.com/CompilandoConocimiento/Reference",
+    CheckOut: "https://compilandoconocimiento.github.io/Reference/",
+    Color: "blue-grey darken-3",
+    English: (
+      <span>
+        This is all my personal implementations of the most famous and useful competitive
+        programming algorithms.
+        <br />
+        <br />
+        Most of the implementations are in C++, but you can also find them in C and even in Python
+        and Java.
+        <br />
+        <br />
+        I'm also working on creating a Reference (Book & Website) so many more people can benefit
+        from it, and get further along in this awesome mind-sport.
+      </span>
+    ),
+    Spanish: (
+      <span>
+        Recopilación de mis implementaciones personales de los más famosos y útiles algoritmos de la
+        programación competitiva.
+        <br />
+        <br />
+        La mayoría de las implementaciones están en C ++, pero también puedes encontrar algunas en C
+        e incluso en Python y Java.
+        <br />
+        <br />
+        También estoy trabajando en la creación de un "Refence" (libro y sitio web) para que así más
+        personas puedan beneficiarse de esto y avanzar más en este increíble deporte mental.
+      </span>
+    ),
+    Tags: {
+      English: ["Competitive Programming", "C++", "Webapp", "React", "Typescript", "Book"],
+      Spanish: ["Programación Competitiva", "C++", "Webapp", "React", "Typescript", "Libro"],
     },
   },
 }
