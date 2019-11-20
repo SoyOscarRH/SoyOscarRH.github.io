037b3c581758cd90610d9c7fb34111d326f4797f
SoyOscarRH <soyoscarrh@gmail.com>
2019-11-20 16:23:26 -0600

    Update to remove key

--- Webpage/Code/Footer.tsx
+++ Webpage/Code/Footer.tsx
@@ -1,32 +1,25 @@
 import React from "react"
 
-export function daysSince(Date1: Date) {
+const daysSince = (date: Date) => {
   const milisecondsInADay = 1000 * 60 * 60 * 24
-  const date1 = Date1.getTime(),
-    date2 = Date.now()
-  const differenceMs = Math.abs(date1 - date2)
+  const date1 = date.getTime()
+  const date2 = Date.now()
 
-  return Math.round(differenceMs / milisecondsInADay)
+  return Math.round(date2 - date1 / milisecondsInADay)
 }
 
-export default function Footer() {
-  return (
-    <footer className="page-footer blue-grey darken-3">
-      <br />
-      <br />
+const Footer = () => (
+  <footer className="page-footer blue-grey darken-3">
+    <br />
+    <br />
 
-      <div className="footer-copyright blue-grey darken-4">
-        <div className="container">
-          Made by Oscar Andrés Rosas Hernandez. {daysSince(new Date("03/23/1998"))} days alive
-        </div>
-
-        <div
-          className="container"
-          onClick={() => (window.location.href = "http://soyoscarrh.github.io/Andres_public.keys")}
-        >
-          Keys
-        </div>
+    <div className="footer-copyright blue-grey darken-4">
+      <div className="container">
+        Made by Oscar Andrés Rosas Hernandez. {daysSince(new Date("03/23/1998"))} days alive
       </div>
-    </footer>
-  )
-}
+    </div>
+  </footer>
+)
+
+export { daysSince }
+export default Footer
