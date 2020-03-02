63d4fce0267d827aeabdb7f6030a4d7c2dba2c36
SoyOscarRH <soyoscarrh@gmail.com>
2020-03-01 20:44:35 -0600

    Update Comment.java
    A simpler version to get the current time

--- Portfolio/src/main/java/com/google/sps/servlets/Comment.java
+++ Portfolio/src/main/java/com/google/sps/servlets/Comment.java
@@ -12,6 +12,6 @@ public class Comment {
   Comment(String username, String message) {
     this.username = username;
     this.message = message;
-    this.time = new Date().getTime();
+    this.time = System.currentTimeMillis();
   }
 }
