40a043b2a3e84c11f44013cac299f03c4cbbe762
SoyOscarRH <soyoscarrh@gmail.com>
2020-10-18 16:41:30 -0500

    Updates

---
+++ precache-manifest.ca4b09203ed4290fb740e4188c8194ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90339b8ab74d39f5f5f89e62a23d62cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d7c3860596c6364a44e",
+    "url": "/static/css/main.12548caf.chunk.css"
+  },
+  {
+    "revision": "08542679994f449419be",
+    "url": "/static/js/2.2e17f450.chunk.js"
+  },
+  {
+    "revision": "20da1371f36bdba5e36ead91be246c0d",
+    "url": "/static/js/2.2e17f450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d7c3860596c6364a44e",
+    "url": "/static/js/main.9129c471.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  }
+]);
